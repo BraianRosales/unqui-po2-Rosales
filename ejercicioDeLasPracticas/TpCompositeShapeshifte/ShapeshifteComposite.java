6bc4eed3d4ecfc6acdd4525f1f278978f1c0e753
braian <braian@furia>
2020-06-19 21:16:17 -0300

    compositeFalla

--- ejercicioDeLasPracticas/TpCompositeShapeshifte/ShapeshifteComposite.java
+++ ejercicioDeLasPracticas/TpCompositeShapeshifte/ShapeshifteComposite.java
@@ -45,6 +45,7 @@ public class ShapeshifteComposite implements IShapeshifte{
 	}
 
 	
+	
 	@Override
 	public int deepest() {
 		
--- test/tpCompositeShapeshifteTest/ShapeshifterTest.java
+++ test/tpCompositeShapeshifteTest/ShapeshifterTest.java
@@ -10,16 +10,28 @@ import TpCompositeShapeshifte.ShapeshifteLeaft;
 
 class ShapeshifterTest {
 
+	
 	ShapeshifteLeaft a;
 	ShapeshifteLeaft b;
+	ShapeshifteLeaft a2;
+	ShapeshifteLeaft b2;
 	ShapeshifteComposite c;
 	ShapeshifteComposite c2;
+	ShapeshifteLeaft d;
+	ShapeshifteComposite z;
+	ShapeshifteComposite z2;
+	ShapeshifteComposite e;
+	ShapeshifteComposite e2;
+	ShapeshifteComposite f;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		
 		this.a = new ShapeshifteLeaft();
 		this.b = new ShapeshifteLeaft();
+		this.a2 = new ShapeshifteLeaft();
+		this.b2 = new ShapeshifteLeaft();
+		this.d = new ShapeshifteLeaft();
 		
 	}
 
@@ -50,10 +62,11 @@ class ShapeshifterTest {
 			}
 	
 	@Test
-	void testCantidadDeElementosDelCompositoCEsIgualALaCantidadDeElemntosDeC2(){
+	void testCantidadDeShapeshifterDelCompuestoCEsIgualALaCantidadDeShipeshiftersDelCompuestoC2(){
 		
 		a.getListaDeElementos().add(1);
 		b.getListaDeElementos().add(2);
+		
 		c = (ShapeshifteComposite)a.compose(b);
 		
 		c2 = new ShapeshifteComposite();
@@ -62,4 +75,61 @@ class ShapeshifterTest {
 		
 		assertEquals(c.getListaDeIshapeshifte(),c2.getListaDeIshapeshifte());
 	}
+	
+	@Test
+	void testLaCantidadDeShapeshifterDelCompositeZEsDos() {
+		a.getListaDeElementos().add(1);
+		b.getListaDeElementos().add(2);
+		d.getListaDeElementos().add(3);
+		
+		c = (ShapeshifteComposite)a.compose(b);
+		z = (ShapeshifteComposite)d.compose(c);
+		
+		assertEquals(z.getListaDeIshapeshifte().size(),2);
+	}
+	
+	@Test
+	void testLaCantidadDeShapeshiterDelCompositeZEsIgualALaCantidadDeShapeshifterDelCompositeDeDComposeC(){               
+		
+		a.getListaDeElementos().add(1);
+		b.getListaDeElementos().add(2);
+		d.getListaDeElementos().add(3);
+		
+		c = (ShapeshifteComposite)a.compose(b);
+		z = (ShapeshifteComposite)d.compose(c);
+		
+		z2= new ShapeshifteComposite();
+		z2.compose(d);
+		z2.compose(c);
+		
+		assertEquals(z.getListaDeIshapeshifte().size(),2);
+		assertEquals(z2.getListaDeIshapeshifte().size(),2);
+		assertEquals(z.getListaDeIshapeshifte(),z2.getListaDeIshapeshifte());
+	}
+	
+	
+	@Test
+	void testLaCantidadDeShapeshifterDelCompositeFEsDos() {
+		
+		a.getListaDeElementos().add(1);
+		b.getListaDeElementos().add(2);
+		d.getListaDeElementos().add(3);
+		a2.getListaDeElementos().add(5);
+		b2.getListaDeElementos().add(6);
+		
+		c = (ShapeshifteComposite)a.compose(b);
+		z = (ShapeshifteComposite)d.compose(c);
+		e = (ShapeshifteComposite)a2.compose(b2);
+		
+		assertEquals(z.getListaDeIshapeshifte().size(),2);
+		assertEquals(e.getListaDeIshapeshifte().size(),2);
+		
+		f = (ShapeshifteComposite)z.compose(e);
+		assertEquals(z.getListaDeIshapeshifte().size(),2);
+	}
+	
 }
+
+
+
+
